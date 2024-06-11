096ea5730381ae6faef130339481ecac5f43bdde
Vitaly Gulyaev <VitalyG-17@yandex.ru>
2024-06-11 13:17:11 +0300

    completed 1 task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'TNtasks';
+  protected count: number = 0;
+
+  protected updateCounter(increment: boolean): void {
+    this.count += increment ? 1 : -1;
+  }
 }
