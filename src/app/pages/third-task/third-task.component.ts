808bb55b0ba2d69794ba7c2e1999c0788e76de41
Vitaly Gulyaev <VitalyG-17@yandex.ru>
2024-06-13 17:11:20 +0300

    fix 3 task

--- src/app/pages/third-task/third-task.component.ts
+++ src/app/pages/third-task/third-task.component.ts
@@ -1,19 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpService, User} from 'src/app/services/http.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-third-task',
   templateUrl: './third-task.component.html',
   styleUrls: ['./third-task.component.scss'],
+  providers: [HttpService],
 })
-export class ThirdTaskComponent implements OnInit {
+export class ThirdTaskComponent implements OnInit, OnDestroy {
   protected users: User[] = [];
+  private subscription: Subscription = new Subscription();
 
-  constructor(private httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService) {}
 
   public ngOnInit(): void {
-    this.httpService.getData().subscribe((users: User[]): void => {
+    this.subscription = this.httpService.getData().subscribe((users: User[]): void => {
       this.users = users;
     });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,18 +3,22 @@ import {HttpClient} from '@angular/common/http';
 import {catchError, Observable, of} from 'rxjs';
 
 export class User {
-  constructor(
-    public id: number,
-    public name: string,
-    public surname: string,
-    public age: number,
-    public hobby: string[],
-  ) {}
+  public id: number;
+  public name: string;
+  public surname: string;
+  public age: number;
+  public hobby: string[];
+
+  constructor(id: number, name: string, surname: string, age: number, hobby: string[]) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.hobby = hobby;
+  }
 }
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class HttpService {
   private readonly url: string = 'http://localhost:3000/users';
 
