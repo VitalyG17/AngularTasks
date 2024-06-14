f8ac0a128f93a5d8c1adf1870f1d3bb47a384079
Vitaly Gulyaev <VitalyG-17@yandex.ru>
2024-06-14 10:42:11 +0300

    completed 4 task

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {catchError, Observable, of} from 'rxjs';
+import {catchError, map, Observable, of} from 'rxjs';
 
 export class User {
   public id: number;
@@ -8,13 +8,25 @@ export class User {
   public surname: string;
   public age: number;
   public hobby: string[];
+  public address: {id: number; street: string; numberHome: number};
 
-  constructor(id: number, name: string, surname: string, age: number, hobby: string[]) {
+  constructor(
+    id: number,
+    name: string,
+    surname: string,
+    age: number,
+    hobby: string[],
+    address: {id: number; street: string; numberHome: number},
+  ) {
     this.id = id;
     this.name = name;
     this.surname = surname;
     this.age = age;
     this.hobby = hobby;
+    this.address = address;
+  }
+  public getAdress(): string {
+    return `${this.address.street} ${this.address.numberHome}`;
   }
 }
 
@@ -26,6 +38,9 @@ export class HttpService {
 
   public getData(): Observable<User[]> {
     return this.http.get<User[]>(this.url).pipe(
+      map((users: User[]) =>
+        users.map((user: User) => new User(user.id, user.name, user.surname, user.age, user.hobby, user.address)),
+      ),
       catchError((err: unknown) => {
         console.error('Error!', err);
         return of([]);
